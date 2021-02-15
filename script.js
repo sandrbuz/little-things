f966d07e58bee84512a2b75aa1c1afb36d2eb4c5
sandrbuz <sandrbuz@mail.ru>
2021-02-15 08:56:55 +0300

    change the title text

--- script.js
+++ script.js
@@ -8,19 +8,24 @@
 // };
 
 // test1(5, 'fsdfs');
+// ----------------------------------------------------------------------------------------------
 
-
-let test = function (name) {
-    if (name.length < 5) {
-        return;
-    }
-    return 'пятая буква вашего имени:' + name[4] + '.';
-};
-
-
-
-console.log(test('Николай'));
+// let test = function (name) {
+//     if (name.length < 5) {
+//         return;
+//     }
+//     return 'пятая буква вашего имени:' + name[4] + '.';
+// };
 
 
 
+// console.log(test('Николай'));
 
+
+// ------------------------------------------------------------------------------------------------------
+
+var headEl = document.getElementById("main-heading");
+
+var tester = prompt('введите новый заголовок:');
+
+headEl.innerHTML = tester;
