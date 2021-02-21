a27c859d24a28d008e903cd15a2c70d2ee512138
sandrbuz <sandrbuz@mail.ru>
2021-02-21 10:21:52 +0300

    made a dropdown menu with arrows

--- script.js
+++ script.js
@@ -76,9 +76,62 @@
 // сначала заголовок сворачивает наверх, а потом разворачивает вниз 
 // ------------------------------------------------------------------------------------------
 
-let counter = 1;
-let test = function () {
-    console.log('ты смотришь уже ' + counter + ' сек.');
-    counter++;
+// let counter = 1;
+// let test = function () {
+//     console.log('ты смотришь уже ' + counter + ' сек.');
+//     counter++;
+// }
+// setInterval(test, 1000);
+
+
+// запускать функцию test каждую секунду
+// ------------------------------------------------------------------------------------------------
+// let out = function () {
+//     document.getElementById('main-heading').style.opacity = '0';
+// }
+// let up = function () {
+//     document.getElementById('main-heading').style.opacity = '1';
+// }
+
+// let heading = document.getElementById('main-heading');
+// heading.addEventListener('mousedown', out);
+// heading.addEventListener('mouseup', up);
+
+
+// исчезновение элемента при нажатии, появление при отжатии мыши
+// -------------------------------------------------------------------------------------------
+
+let pointt1 = document.querySelector('.point1');
+let pointt2 = document.querySelector('.point2');
+let pointt3 = document.querySelector('.point3');
+let pointt4 = document.querySelector('.point4');
+
+let menu = document.getElementById('wrapperr');
+
+let ar1 = document.getElementById('arr1');
+let ar2 = document.getElementById('arr2');
+
+let pointsIn = function () {
+    pointt1.classList.add('open');
+    pointt2.classList.add('open');
+    pointt3.classList.add('open');
+    pointt4.classList.add('open');
+    ar1.classList.add('close-arr');
+    ar2.classList.add('open-arr');
+
+
 }
-setInterval(test, 1000);
+
+let pointsOut = function () {
+    pointt1.classList.remove('open');
+    pointt2.classList.remove('open');
+    pointt3.classList.remove('open');
+    pointt4.classList.remove('open');
+    ar1.classList.remove('close-arr');
+    ar2.classList.remove('open-arr');
+}
+
+
+menu.addEventListener('mouseover', pointsIn);
+menu.addEventListener('mouseout', pointsOut);
+
